89afb902bf952c75ea2b77d3c7445a14b7433727
sii2017 <30471043+sii2017@users.noreply.github.com>
2017-11-04 12:35:03 +0800

    Chapter 7 Update

--- Chapter 7/CHECKER4/CHECKER4.c
+++ Chapter 7/CHECKER4/CHECKER4.c
@@ -77,7 +77,40 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 		MessageBeep(0);
 		return 0;
 	case WM_SETFOCUS:
-		SetFocus(GetDlgItem(hwnd, idFocus));	//写到这里
+		SetFocus(GetDlgItem(hwnd, idFocus));	//？？
+		return 0;
+	case WM_KEYDOWN:
+		x= idFocus & 0xFF;
+		y= idFocus >> 8;
+
+		switch(wParam)
+		{
+		case VK_UP:
+			y--;
+			break;
+		case VK_DOWN:
+			y++;
+			break;
+		case VK_LEFT:
+			x--;
+			break;
+		case VK_RIGHT:
+			x++;
+			break;
+		case VK_HOME:
+			x=y=0;
+			break;
+		case VK_END:
+			x=y=DIVISIONS-1;
+			break;
+		default:
+			return 0;
+		}
+		x= (x+DIVISIONS)%DIVISIONS;
+		y= (y+DIVISIONS)%DIVISIONS;
+		idFocus= y<<8 |x;
+
+		SetFocus(GetDlgItem(hwnd, idFocus));
 		return 0;
 	case WM_DESTROY:
 		PostQuitMessage(0);
@@ -97,9 +130,21 @@ LRESULT CALLBACK ChildWndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lPa
 	case WM_CREATE:
 		SetWindowLong(hwnd, 0, 0);
 		return 0;
+	case WM_KEYDOWN:
+		if(wParam!=VK_RETURN&&wParam!=VK_SPACE)	//把除了回车和空格的大部分键发送给父窗口
+		{
+			SendMessage(GetParent(hwnd), message, wParam, lParam);
+			return 0;
+		}
 	case WM_LBUTTONDOWN:
-		SetWindowLong(hwnd, 0, 1^GetWindowLong(hwnd, 0));
+		SetWindowLong(hwnd, 0, 1^GetWindowLong(hwnd, 0));	//对左键点击的窗口进行标记
+		SetFocus(hwnd);
 		InvalidateRect(hwnd, NULL, FALSE);
+		return 0;
+	case WM_SETFOCUS:
+		idFocus= GetWindowLong(hwnd, GWL_ID);	//获取子窗口的子ID
+	case WM_KILLFOCUS:
+		InvalidateRect(hwnd, NULL, TRUE);	//失去焦点则重新绘图
 		return 0;
 	case WM_PAINT:
 		hdc= BeginPaint(hwnd, &ps);
@@ -107,12 +152,23 @@ LRESULT CALLBACK ChildWndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lPa
 		GetClientRect(hwnd, &rect);
 		Rectangle(hdc, 0,0, rect.right, rect.bottom);
 
-		if(GetWindowLong(hwnd, 0))
+		if(GetWindowLong(hwnd, 0))	//根据标记画交叉线
 		{
 			MoveToEx(hdc, 0,0, NULL);
 			LineTo(hdc, rect.right, rect.bottom);
 			MoveToEx(hdc, 0, rect.bottom, NULL);
 			LineTo(hdc, rect.right, 0);
+		}
+		if(hwnd==GetFocus())	//如果当前窗口为焦点画一个小方框在内部表示，如果失去焦点了重新画则由于标记还是能画交叉线，但是焦点标识没有了
+		{
+			rect.left+=rect.right/10;
+			rect.right-= rect.left;
+			rect.top+= rect.bottom/10;
+			rect.bottom-= rect.top;
+			SelectObject(hdc, GetStockObject(NULL_BRUSH));
+			SelectObject(hdc, CreatePen(PS_DASH, 0, 0));
+			Rectangle(hdc, rect.left, rect.top, rect.right, rect.bottom);
+			DeleteObject(SelectObject(hdc, GetStockObject(BLACK_PEN)));
 		}
 		EndPaint(hwnd, &ps);
 		return 0;
