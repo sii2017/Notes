ffdc07e0868bb26354bcdefffb4070b71fd48e0d
sii2017 <30471043+sii2017@users.noreply.github.com>
2017-12-08 17:55:09 +0800

    Chapter 10 Update

---
+++ Chapter 10/Chapter 10/POPMENU/resource.h
@@ -0,0 +1,35 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by POPMENU.rc
+//
+#define IDR_MENU1                       101
+#define MyMenu                          102
+#define IDM_FILE_NEW                    40001
+#define IDM_FILE_OPEN                   40002
+#define IDM_FILE_SAVE                   40003
+#define IDM_FILE_SAVE_AS                40004
+#define IDM_APP_EXIT                    40005
+#define IDM_EDIT_UNDO                   40006
+#define ID_EDIT_CUT40007                40007
+#define IDM_EDIT_CUT                    40008
+#define IDM_EDIT_COPY                   40009
+#define IDM_EDIT_PASTE                  40010
+#define IDM_EDIT_CLEAR                  40011
+#define IDM_BKGND_WHITE                 40012
+#define IDM_BKGND_LTGRAY                40013
+#define IDM_BKGND_GRAY                  40014
+#define IDM_BKGND_DKGRAY                40015
+#define IDM_BKGND_BLACK                 40016
+#define IDM_APP_HELP                    40017
+#define IDM_APP_ABOUT                   40018
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40019
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
