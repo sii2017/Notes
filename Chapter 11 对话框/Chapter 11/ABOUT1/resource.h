ea5e5030357ecfce83351deb838a59f591f3a11c
sii2017 <30471043+sii2017@users.noreply.github.com>
2017-12-21 14:53:43 +0800

    Chapter 11 Update

---
+++ Chapter 11 对话框/Chapter 11/ABOUT1/resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ABOUT1.rc
+//
+#define IDD_DIALOG1                     101
+#define IDR_MENU1                       103
+#define ID_HELP_ABOUT                   40001
+#define IDM_APP_ABOUT                   40002
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40003
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
