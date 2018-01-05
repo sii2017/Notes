a82fc642dd972ccd2d5faad10ce37967ecafd7c2
sii2017 <30471043+sii2017@users.noreply.github.com>
2018-01-05 17:49:29 +0800

    Chapter 11 Update

---
+++ Chapter 11 对话框/Chapter 11/POPPAD3/resource.h
@@ -0,0 +1,36 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by POPPAD3.rc
+//
+#define IDD_DIALOG1                     104
+#define IDM_FILE_NEW                    40001
+#define IDM_FILE_OPEN                   40002
+#define IDM_FILE_SAVE                   40003
+#define IDM_FILE_SAVE_AS                40004
+#define IDM_FILE_PRINT                  40005
+#define IDM_APP_EXIT                    40006
+#define IDM_EDIT_UNDO                   40007
+#define IDM_EDIT_CUT                    40008
+#define IDM_EDIT_COPY                   40009
+#define IDM_EDIT_PASTE                  40010
+#define IDM_EDIT_CLEAR                  40011
+#define ID_EDIT_SELETEALL               40012
+#define IDM_EDIT_SELECT_ALL             40013
+#define IDM_SERACH_FIND                 40014
+#define IDM_SEARCH_REPLACE              40015
+#define IDM_FORMAT_FONT                 40016
+#define IDM_HELP                        40017
+#define IDM_APP_ABOUT                   40018
+#define IDM_SEARCH_NEXT                 40023
+#define IDM_SEARCH_FIND                 40027
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40037
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
